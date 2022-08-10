d6a75a8d0f7134097672ab5124c982b428bc2468
ne5s <phobos902080@gmail.com>
2022-08-11 02:59:02 +0900

    [Silver V] Title: 셀프 넘버, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,19 +1,8 @@
-def self_number(n):
-    if(n < 10):
-        return n+n
-    elif(n>=10 and n<100):
-        return n + int(str(n)[0]) + int(str(n)[1])
-    elif(n>=100 and n<1000):
-        return n + int(str(n)[0]) + int(str(n)[1]) + int(str(n)[2])
-    elif(n>=1000 and n<10000):
-        return n + int(str(n)[0]) + int(str(n)[1]) + int(str(n)[2]) + int(str(n)[3])
-    elif(n == 10000):
-        return n + int(str(n)[0]) + int(str(n)[1]) + int(str(n)[2]) + int(str(n)[3]) + int(str(n)[4])
-    
-self_number_li = []
-for i in range(1, 10001):
-    self_number_li.append(self_number(i))
-for j in range(1, 10001):
-    if j not in self_number_li:
-        print(j)
+result = [True]*11000
 
+for i in range(1, 10000):
+    result[(i + i//1000 + i%1000//100 + i%100//10 + i%10)] = False
+
+for i in range(1, 10000):
+    if result[i]:
+        print(i)

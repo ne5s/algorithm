381706800d42587b3b4eff6d3a77a4161ef66165
ne5s <phobos902080@gmail.com>
2022-08-12 02:38:40 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,14 @@
+def solution(id_list, report, k):
+    l = len(id_list)
+    answer = [0]*l
+    report = list(set(report))
+    singo = [] # 그림 1
+    singo_count = [0]*l
+    for i in report:
+        user, singos = i.split()
+        singo.append([user, singos])
+        singo_count[id_list.index(singos)] += 1
+    for u,s in singo:
+        if singo_count[id_list.index(s)] >= k:
+            answer[id_list.index(u)] += 1
+    return answer

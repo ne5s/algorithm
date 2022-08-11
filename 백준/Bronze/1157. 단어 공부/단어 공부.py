a67c0ba12b1e590ff4a70fbabd4cdcfa5dfa93c4
ne5s <phobos902080@gmail.com>
2022-08-11 17:15:52 +0900

    [Bronze I] Title: 단어 공부, Time: 96 ms, Memory: 32796 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,13 +1,9 @@
 from sys import stdin
 
-a = stdin.readline().rstrip()
-a = a.upper()
-li = [0]*26
-for i in a:
-    li[(ord(i)-65)] += 1
-m = max(li)
-max_index = [i for i,v in enumerate(li) if v == m]
-if len(max_index) >= 2:
-    print("?")
-else:
-    print(chr(li.index(m)+65))
+i=stdin.readline().rstrip().upper()
+n=0
+for p in map(chr,range(65,91)):
+  q=i.count(p)
+  if n<q:n,c=q,p
+  elif n==q:c="?"
+print(c)

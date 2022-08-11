22aa73af1944c3a901af331b1f3cd46bc7afdeee
ne5s <phobos902080@gmail.com>
2022-08-11 16:39:32 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,12 +1,6 @@
 from sys import stdin
 
 A, B = stdin.readline().split()
-reversed_A = ''
-reversed_B = ''
-for i in reversed(range(len(A))):
-    reversed_A += A[i]
-    reversed_B += B[i]
-if int(reversed_A) > int(reversed_B):
-    print(reversed_A)
-else:
-    print(reversed_B)
+reversed_A = A[::-1]
+reversed_B = B[::-1]
+print(max(reversed_A, reversed_B))

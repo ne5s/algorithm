eaa005f75aab7f52fcd0c6a02d01c142e910bcff
ne5s <phobos902080@gmail.com>
2022-08-11 22:03:25 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -4,19 +4,7 @@ N = int(stdin.readline())
 sum = 0
 for i in range(N):
     a = stdin.readline().rstrip()
-    li = []
-    flag = True
-    for j in range(len(a)):
-        if j == 0:
-            li.append(a[j])
-            continue
-        if j != 0 and a[j] not in li:
-            li.append(a[j])
-        elif j != 0 and a[j] in li and a[j-1] == a[j]:
-            li.append(a[j])
-        else:
-            flag = False
-            break
-    if flag:
+    if list(a) == sorted(a, key=a.find):
         sum += 1
 print(sum)
+

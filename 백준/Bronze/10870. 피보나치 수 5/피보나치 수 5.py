3bdd0f0385585419e429b887a4193766808fc8a5
ne5s <phobos902080@gmail.com>
2022-08-13 20:54:56 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,10 @@
+from sys import stdin
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+n = int(stdin.readline())
+print(fibo(n))

a3803b60b4a14016bae5aedf84d41713b4eaa24d
ne5s <phobos902080@gmail.com>
2022-08-21 20:22:07 +0900

    [Silver V] Title: 숫자 카드, Time: 500 ms, Memory: 123712 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -2,28 +2,7 @@ from sys import stdin
 
 n = int(stdin.readline())
 sol = list(map(int, stdin.readline().split()))
-if len(sol) != n:
-    print("잘못된 입력입니다.")
+sol = set(sol)
 m = int(stdin.readline())
 ipt = list(map(int, stdin.readline().split()))
-if len(ipt) != m:
-    print("잘못된 입력입니다.")
-    
-sol.sort()
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return None
-    
-for i in range(m):
-    if binary_search(sol, ipt[i], 0, n-1) is not None:
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+print(" ".join(map(lambda x: "1" if x in sol else "0", ipt)))

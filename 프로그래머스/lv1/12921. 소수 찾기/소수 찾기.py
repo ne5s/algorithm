5b7bc1d7d70904e4627cf3841cb274434e88038a
ne5s <phobos902080@gmail.com>
2022-08-24 18:26:46 +0900

    [level 1] Title: 소수 찾기, Time: 2957.93 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -7,7 +7,7 @@ def solution(n):
         if i == 1:
             continue
         else:
-            for j in range(2, int(math.sqrt(i))+1):
+            for j in range(2, int(i**0.5)+1):
                 if i % j == 0:
                     flag = 1
                     break

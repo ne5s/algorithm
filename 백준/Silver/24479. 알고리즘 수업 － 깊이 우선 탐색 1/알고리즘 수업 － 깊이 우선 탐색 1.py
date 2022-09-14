52f03f3d1120251c67cf7606cb97cdb0bb951417
ne5s <phobos902080@gmail.com>
2022-09-14 19:11:35 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 620 ms, Memory: 160264 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,43 +1,30 @@
+from sys import stdin
 import sys
-sys.setrecursionlimit(10 ** 6)  # 재귀 허용 깊이를 수동으로 늘려주는 코드
-# 정점의 수,간선의 수,시작 정점
-n, m, r = map(int, sys.stdin.readline().split())
-# 연결노드 그래프 초기화(1번노드와 인덱스 값이 같게 하기 위해서 n+1로 )
-# [[],[],[],[],[],[]]
-graph = [[] for _ in range(n + 1)]
-# 방문 순서 그래프 (이것도 인덱스 값과 노드의 값이 동일하게 만드릭 위해서 설정 )
-visted = [0] * (n + 1)
-# 순차 입력
-cnt = 1
+sys.setrecursionlimit(10 ** 6)
 
-
-def dfs(graph, v, visted):
-    # 함수 밖에 cnt값을 쓰기 위해서 global이라고 명시
-    global cnt
-    # 방문할 때마다 순차 값 변경
-    visted[v] = cnt
-    # 연결된 노드 방문
+def dfs(graph, v, visited):
+    global order
+    visited[v] = order
     for i in graph[v]:
-        # 방문 안한 노드일 경우
-        if visted[i] == 0:
-            # 순차 증가
-            cnt += 1
-            # dfs 실행
-            dfs(graph, i, visted)
+        if visited[i] == 0:
+            order += 1
+            dfs(graph, i, visited)
 
+n, m, r = map(int, stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+order = 1
 
-# 연결된 노드 입력 받기
-for i in range(m):
-    u, v = map(int, sys.stdin.readline().split())
+for _ in range(m):
+    u, v = map(int, stdin.readline().split())
     graph[u].append(v)
     graph[v].append(u)
 
-# 오름차순 정리
-for i in range(n + 1):
+for i in range(len(graph)):
     graph[i].sort()
 
-dfs(graph, r, visted)
-# 순차 출력
-for i in range(n + 1):
-    if i != 0:
-        print(visted[i])
+
+dfs(graph, r, visited)
+
+for i in range(1, len(visited)):
+    print(visited[i])

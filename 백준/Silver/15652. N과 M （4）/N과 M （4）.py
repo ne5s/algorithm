bd0a028165474fa4549f3cb19a6164d65595f2b7
ne5s <phobos902080@gmail.com>
2022-09-28 05:53:49 +0900

    [Silver III] Title: N과 M (4), Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+res = [] # 출력 내용
+
+def dfs(depth, n, m, index):
+    if depth == m: # 탈출 조건
+        print(' '.join(map(str, res))) # list를 str으로 합쳐 출력
+        return
+    for i in range(index, n): # 탐사 check 하면서
+        res.append(i+1) # 탐사 내용
+        dfs(depth+1, n, m, i) # 깊이 우선 탐색
+        res.pop() # 탐사 내용 제거
+
+dfs(0, n, m, 0)

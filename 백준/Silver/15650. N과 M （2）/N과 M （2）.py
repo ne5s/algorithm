dc0f98c0e8e44be1a4cd94519db50bb44a53bb7a
ne5s <phobos902080@gmail.com>
2022-09-28 05:13:36 +0900

    [Silver III] Title: N과 M (2), Time: 148 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+visited = [False] * n # 탐사 여부 check
+res = [] # 출력 내용
+
+def dfs(depth, n, m):
+    if depth == m: # 탈출 조건
+        if depth == 1:
+            for i in range(len(res)):
+                print(' '.join(map(str, res)))  # list를 str으로 합쳐 출력
+            return
+        flag = 0
+        for i in range(0, len(res)-1):
+            if res[i] > res[i+1]:
+                flag = 1
+                break
+        if not flag:
+            print(' '.join(map(str, res))) # list를 str으로 합쳐 출력
+        return
+    for i in range(n): # 탐사 check 하면서
+        if not visited[i]: # 탐사 안했다면
+            visited[i] = True # 탐사 시작(중복 제거)
+            res.append(i+1) # 탐사 내용
+            dfs(depth+1, n, m) # 깊이 우선 탐색
+            visited[i] = False # 깊이 탐사 완료
+            res.pop() # 탐사 내용 제거
+
+
+dfs(0, n, m)

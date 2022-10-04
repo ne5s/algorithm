d6f71b27080fea8d5ee3761bd9e4d4710b81571e
ne5s <phobos902080@gmail.com>
2022-10-04 22:19:05 +0900

    [Silver V] Title: 알고리즘 수업 - 힙 정렬 1, Time: 4976 ms, Memory: 89460 KB -BaekjoonHub

--- 백준/Silver/24173. 알고리즘 수업 － 힙 정렬 1/알고리즘 수업 － 힙 정렬 1.py
+++ 백준/Silver/24173. 알고리즘 수업 － 힙 정렬 1/알고리즘 수업 － 힙 정렬 1.py
@@ -1,32 +1,25 @@
 from sys import stdin
 
-n, k = map(int, stdin.readline().split())
-li = [0]
-a = list(map(int, stdin.readline().split()))
-for i in a:
-    li.append(i)
-cnt = 0
-small = []
-
-def heap_sort(li):
+def heap_sort(li, n):
     global cnt
-    build_min_heap(li)
+    build_min_heap(li, n)
     for i in range(n, 1, -1):
-        cnt += 1
-        small.append([min(li[i], li[1]), max(li[i], li[1])])
         li[1], li[i] = li[i], li[1]
+        cnt += 1
+        if cnt == K:
+            print(li[i], li[1])
+            exit()
         heapify(li, 1, i-1)
 
-def build_min_heap(li):
+def build_min_heap(li, n):
     for i in range(n//2, 0, -1):
         heapify(li, i, n)
 
 def heapify(li, k, n):
     global cnt
-    global small
     left = 2 * k
     right = 2 * k + 1
-    if (right <= n):
+    if right <= n:
         if li[left] < li[right]:
             smaller = left
         else:
@@ -37,15 +30,15 @@ def heapify(li, k, n):
         return
 
     if li[smaller] < li[k]:
-        cnt += 1
-        small.append([li[smaller], li[k]])
         li[k], li[smaller] = li[smaller], li[k]
+        cnt += 1
+        if cnt == K:
+            print(li[k], li[smaller])
+            exit()
         heapify(li, smaller, n)
 
-    return
-
-heap_sort(li)
-if k > cnt:
-    print(-1)
-else:
-    print(small[k-1][0], small[k-1][1])
+N, K = map(int, stdin.readline().split())
+li = [0] + list(map(int, stdin.readline().split()))
+cnt = 0
+heap_sort(li, N)
+print(-1)

0bb3700243a93d14f7a38c776f5cd582ff8b65d9
ne5s <phobos902080@gmail.com>
2022-10-31 01:51:18 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 688 ms, Memory: 160260 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,30 @@
+from sys import stdin
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+def dfs(graph, v, visited):
+    global order
+    visited[v] = order
+    for i in graph[v]:
+        if visited[i] == 0:
+            order += 1
+            dfs(graph, i, visited)
+
+n, m, r = map(int, stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+order = 1
+
+for _ in range(m):
+    u, v = map(int, stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(len(graph)):
+    graph[i].sort(reverse=True)
+
+
+dfs(graph, r, visited)
+
+for i in range(1, len(visited)):
+    print(visited[i])

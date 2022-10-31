37f54e782e49a714f5f585f69753b107462758c5
ne5s <phobos902080@gmail.com>
2022-10-31 22:16:37 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 624 ms, Memory: 58004 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,35 @@
+from sys import stdin
+from collections import deque
+
+def bfs(start):
+    global order
+    visited[start] = order
+    dq.append(start)
+    while len(dq) != 0:
+        node = dq.popleft()
+        for adj in graph[node]:
+            if not visited[adj]:
+                order += 1
+                visited[adj] = order
+                dq.append(adj)
+
+
+n, m, r = map(int, stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+dq = deque()
+visited = [0] * (n+1)
+order = 1
+
+for _ in range(m):
+    u, v = map(int, stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(len(graph)):
+    graph[i].sort(reverse=True)
+
+bfs(r)
+
+for i in range(1, len(visited)):
+    print(visited[i])
+    

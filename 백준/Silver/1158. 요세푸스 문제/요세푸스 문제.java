f11cd70c66332f5164803b8e51fcee8b2d30023c
ne5s <phobos902080@gmail.com>
2024-06-24 22:10:58 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 164 ms, Memory: 14792 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputReader ir = new InputReader(System.in);
+        int N = ir.nextInt();
+        int K = ir.nextInt();
+        List<Integer> list = new LinkedList<>();
+        StringBuffer sb = new StringBuffer("<");
+
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+        int num = 0;
+        while (!list.isEmpty()) {
+            num = (num + (K - 1)) % list.size();
+            sb.append(list.remove(num));
+            sb.append(", ");
+        }
+        sb.replace(sb.length() - 2, sb.length(), ">");
+        System.out.println(sb.toString());
+    }
+
+    static class InputReader {private final InputStream stream;private final byte[] buf = new byte[8192];private int curChar, snumChars;public InputReader(InputStream st) {this.stream = st;}public int read() {if (snumChars == -1)throw new InputMismatchException();if (curChar >= snumChars) {curChar = 0;try {snumChars = stream.read(buf);} catch (
+            IOException e) {throw new InputMismatchException();}if (snumChars <= 0)return -1;}return buf[curChar++];}public int nextInt() {int c = read();while (isSpaceChar(c))c = read();int sgn = 1;if (c == '-') {sgn = -1;c = read();}int res = 0;do {res *= 10;res += c - '0';c = read();} while (!isSpaceChar(c));return res * sgn;}public long nextLong() {int c = read();while (isSpaceChar(c))c = read();int sgn = 1;if (c == '-') {sgn = -1;c = read();}long res = 0;do {res *= 10;res += c - '0';c = read();} while (!isSpaceChar(c));return res * sgn;}public int[] nextIntArray(int n) {int a[] = new int[n];for (int i = 0; i < n; i++)a[i] = nextInt();return a;}public List<Integer> nextIntList(int n) {List<Integer> a = new ArrayList<>();for (int i = 0; i < n; i++)a.add(nextInt());return a;}public String readString() {int c = read();while (isSpaceChar(c)) {c = read();}StringBuilder res = new StringBuilder();do {res.appendCodePoint(c);c = read();} while (!isSpaceChar(c));return res.toString();}public String nextLine() {int c = read();while (isSpaceChar(c))c = read();StringBuilder res = new StringBuilder();do {res.appendCodePoint(c);c = read();} while (!isEndOfLine(c));return res.toString();}public boolean isSpaceChar(int c) {return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;}private boolean isEndOfLine(int c) {return c == '\n' || c == '\r' || c == -1;}}
+}
